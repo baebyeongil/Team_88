b3400212f0332dbfa1da586582fabb0e2ec8dc2d
mingyu kim9411 <awer159@daum.net>
2023-08-08 10:37:58 +0900

    특정보드 불러오기

--- routes/board.router.js
+++ routes/board.router.js
@@ -3,12 +3,12 @@ const auth = require('../middleware/auth.js');
 const BoardController = require('../controllers/board.controller');
 const boardController = new BoardController();
 
-
-router.post('/',auth, boardController.createBoard);
-router.get('/',auth, boardController.getBoardList);
-router.get('/:boardId',auth, boardController.getBoard);
-router.put('/:boardId',auth, boardController.updateBoard);
-router.delete('/:boardId',auth, boardController.deleteBoard);
+router.post('/', auth, boardController.createBoard);
+router.get('/', auth, boardController.getBoardList);
+router.post('/:boardId', boardController.inviteBoard);
+router.get('/:boardId', auth, boardController.getBoard);
+router.put('/:boardId', auth, boardController.updateBoard);
+router.delete('/:boardId', auth, boardController.deleteBoard);
 // router.post('/:boardId/member', boardController.invateBoard);
 
 module.exports = router;
