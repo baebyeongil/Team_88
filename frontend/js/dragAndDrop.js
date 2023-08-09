dfa1f835b0fda451fb91455b673d40a855aaf781
BAE BYEONGIL <b000202@naver.com>
2023-08-09 15:07:33 +0900

    movecolumn front

--- frontend/js/dragAndDrop.js
+++ frontend/js/dragAndDrop.js
@@ -5,9 +5,7 @@ let dragStartIndex = null;
 // 컬럼 드래그 시작 이벤트 리스너
 columnsContainer.addEventListener('dragstart', event => {
   draggingColumn = event.target;
-  dragStartIndex = Array.from(columnsContainer.children).indexOf(
-    draggingColumn
-  );
+  dragStartIndex = draggingColumn.id;
 });
 
 // 컬럼 드래그 중 이벤트 리스너
@@ -22,43 +20,33 @@ columnsContainer.addEventListener('drop', async event => {
   if (!draggingColumn || dragStartIndex === null) return;
 
   const droppedColumn = event.target;
-  if (droppedColumn.classList.contains('column')) {
-    const dragEndIndex = Array.from(columnsContainer.children).indexOf(
-      droppedColumn
-    );
-    console.log(dragEndIndex);
 
-    if (dragStartIndex !== dragEndIndex) {
-      // 보드 ID 및 컬럼 ID 가져오기
-      const urlParams = new URLSearchParams(window.location.search);
-      const boardId = urlParams.get('id');
-      const columnIdToMove = draggingColumn.dataset.columnId;
+  const dragdropIndex = Array.from(columnsContainer.children).indexOf(
+    droppedColumn
+  );
+  const urlParams = new URLSearchParams(window.location.search);
+  const boardId = urlParams.get('id');
 
-      // 새로운 순서로 컬럼을 이동하는 API 요청 보내기
-      try {
-        const response = await fetch(
-          `/board/${boardId}/column/${columnIdToMove}`,
-          {
-            method: 'PUT', // 변경이 필요한 경우 PUT 또는 다른 HTTP 메서드 사용
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ number: dragEndIndex }),
-          }
-        );
+  // 새로운 순서로 컬럼을 이동하는 API 요청 보내기
+  try {
+    const response = await fetch(`/board/${boardId}/column/${dragStartIndex}`, {
+      method: 'PATCH', // 변경이 필요한 경우 PUT 또는 다른 HTTP 메서드 사용
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ number: dragdropIndex }),
+    });
 
-        if (response.ok) {
-          console.log('Column order updated successfully.');
-          location.reload(); // 컬럼 순서가 변경되었으므로 화면 다시 로드
-        } else {
-          console.error('Failed to update column order.');
-        }
-      } catch (error) {
-        console.error('Error while updating column order:', error);
-      }
+    if (response.ok) {
+      console.log('Column order updated successfully.');
+      location.reload(); // 컬럼 순서가 변경되었으므로 화면 다시 로드
+    } else {
+      console.error('Failed to update column order.');
     }
-
-    draggingColumn = null;
-    dragStartIndex = null;
+  } catch (error) {
+    console.error('Error while updating column order:', error);
   }
+
+  draggingColumn = null;
+  dragStartIndex = null;
 });
