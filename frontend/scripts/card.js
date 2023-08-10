d88b99d2e95b8a783c9df6b6ffa305ccbe8c7fa1
mingyu kim9411 <awer159@daum.net>
2023-08-10 09:55:46 +0900

    card 수정기능 프론트 연결

--- frontend/scripts/card.js
+++ frontend/scripts/card.js
@@ -21,26 +21,33 @@ const getCard = () => {
     const cardContent = datas.result.content;
     const cardId = datas.result.id;
     const worker = datas.result.user.nickname;
+    const deadLine = datas.result.deadLine;
+    const workerEmail = datas.result.user.email;
     $('#cardBox').empty();
     temp.innerHTML = `<div class="card">
-                      <div class="card-header d-flex justify-content-between">
-                      <span class="card-worker">작업자:${worker}</span>
-                      <span>${cardTitle}</span>
-                      <div class="btn-group">
-                      <button id="cardEditBtn" class="btn btn-primary me-2" style="padding: 0; width: 60px; height: 30px; border-radius: 0; display: flex; align-items: center; justify-content: center;" onclick="editCard(${cardId})">수정</button>
-                      <button id="cardDeleteBtn" class="btn btn-primary" style="padding: 0; width: 30px; height: 30px; border-radius: 0; display: flex; align-items: center; justify-content: center;" onclick="deleteCard(${cardId})">x</button>
-                      </div>
-                      </div>
-                      <div class="card-body">
-                      <h5 class="card-title" style="margin-left: 20px;">${cardContent}</h5>
-                      <div class="d-flex justify-content-end">
-                      <button id="submitComment" type="button" class="btn btn-primary me-2" data-bs-toggle="modal" data-bs-target="#commentModal">
-                      댓글
-                      </button>
-                      <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#checklistModal">체크리스트</button>
-                      </div>
-                      </div>
-                      </div>`;
+    <div class="card-header d-flex justify-content-between">
+      <span class="card-worker">작업자:${worker}</span>
+      <div>
+      <span class="card-deadLine">마감일:${formatDate(deadLine)}</span>
+      </div>
+    </div>
+    <div class="card-header d-flex justify-content-between">
+      <span>${cardTitle}</span>
+      <div class="btn-group">
+        <button id="cardEditBtn" class="btn btn-primary me-2" style="padding: 0; width: 60px; height: 30px; border-radius: 0; display: flex; align-items: center; justify-content: center;" onclick="openEditModal(${cardId}, '${cardTitle}', '${cardContent}', '${workerEmail}', '${deadLine}')">수정</button>
+        <button id="cardDeleteBtn" class="btn btn-primary" style="padding: 0; width: 60px; height: 30px; border-radius: 0; display: flex; align-items: center; justify-content: center;" onclick="deleteCard(${cardId})">삭제</button>
+      </div>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title" style="margin-left: 20px;">${cardContent}</h5>
+      <div class="d-flex justify-content-end">
+        <button id="submitComment" type="button" class="btn btn-primary me-2" data-bs-toggle="modal" data-bs-target="#commentModal">
+          댓글
+        </button>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#checklistModal">체크리스트</button>
+      </div>
+    </div>
+  </div>`;
     document.querySelector('#cardBox').append(temp);
   });
 };
@@ -123,6 +130,27 @@ function deleteComment(commentId) {
     });
 }
 
+function upadateCard(newTitle, newContent, newEmail, newDeadLine) {
+  const req = {
+    title: newTitle,
+    content: newContent,
+    email: newEmail,
+    deadLine: newDeadLine,
+  };
+  fetch(`/column/${columnId}/card/${cardId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  })
+    .then(res => res.json())
+    .then(res => {
+      alert(res.result);
+      window.location.reload();
+    });
+}
+
 function deleteCard(cardId) {
   fetch(`/column/${columnId}/card/${cardId}`, {
     method: 'DELETE',
@@ -137,5 +165,40 @@ function deleteCard(cardId) {
       window.location.href = `myboard.html?id=${boardId}`;
     });
 }
+function openEditModal(cardId, title, content, email, deadLine) {
+  $('#editTitleInput').val(title);
+  $('#editContentInput').val(content);
+  $('#editWorkerInput').val(email);
+  $('#editDeadLineInput').val(deadLine);
+
+  $('#saveEditCardBtn')
+    .off('click')
+    .on('click', function () {
+      const newTitle = $('#editTitleInput').val();
+      const newContent = $('#editContentInput').val();
+      const newEmail = $('#editWorkerInput').val();
+      const newDeadLine = $('#editDeadLineInput').val();
+
+      // 여기서 수정 데이터를 처리하는 함수 호출
+      upadateCard(newTitle, newContent, newEmail, newDeadLine);
+
+      // 모달 닫기
+      $('#editCardModal').modal('hide');
+    });
+
+  $('#editCardModal').modal('show');
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+const originalDate = '2023-08-10T00:00:00.000Z';
+const formattedDate = formatDate(originalDate);
+console.log(formattedDate);
 getCard();
 getComment();
